fc6c5e94914815d4c81e1204308e5576634ee089
Achraf Hebboul <adhebboul@gmail.com>
2023-04-06 19:38:08 +0000

    write the core code of links in paginator

--- src/Paginator.php
+++ src/Paginator.php
@@ -19,11 +19,27 @@ class Paginator
         $this->countPerPage = $countPerPage;
         $this->currentPage = $page;
         $this->totalData = count($data);
-        $this->sumPages = (is_float($this->totalData / $this->countPerPage) ? floor($this->totalData / $this->countPerPage) : ($this->totalData / $this->countPerPage) - 1);
+        //$this->sumPages = (is_float($this->totalData / $this->countPerPage) ? floor($this->totalData / $this->countPerPage) : ($this->totalData / $this->countPerPage) - 1);
+        $this->sumPages = ceil($this->totalData / $this->countPerPage);
     }
 
     public function getData(): array
     {
-        return array_slice($this->data, $this->currentPage * $this->countPerPage, $this->countPerPage);
+        return array_slice($this->data, ($this->currentPage - 1) * $this->countPerPage, $this->countPerPage);
+    }
+
+    public function getLinks(): array
+    {
+        $range = range(1, $this->sumPages); // create a range of links, first just numbers between 1 and Count of Pages
+        if (count($range) > 6) {
+            $links = array_slice($range, ((count($range) - $this->currentPage < 5) ? count($range) - 5: $this->currentPage), 5);
+            
+            if ($links[0] > 2) array_unshift($links, 1, '...',);
+            else if ($links[0] == 2) array_unshift($links, 1,);
+
+            if ($links[count($links) - 1] < $range[count($range) - 2]) array_push($links, '...', $range[count($range) - 1]);
+            else if ($links[count($links) - 1] == $range[count($range) - 2]) array_push($links, $range[count($range) - 1]);
+        }
+        return isset($links) ? $links : $range;
     }
 }
